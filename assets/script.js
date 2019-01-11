5df7329c70b40198b1e4b217e14a40eda8f65aca
Meghan Stang <mstang15@gmail.com>
2019-01-11 16:16:33 -0700

    adds script file

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var random_number = randomNumber(1,100);
+
+
+function clearGuess() {
+
+}
+
+function resetGuess() {
+
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max-min) + min );
+}
+
+function startGame() {
+
+}
