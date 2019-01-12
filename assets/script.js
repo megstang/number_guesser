9fcea5542d99684b9041140ee0c1070d268b4d06
Meghan Stang <mstang15@gmail.com>
2019-01-11 17:13:53 -0700

    reset clears all feedback that was displayed

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,7 @@ function clearGuess() {
 
 function resetGuess() {
   random_number = randomNumber(1,100);
+  feedback("clearfeedback", "");
 }
 
 function randomNumber(min, max) {
@@ -17,14 +18,23 @@ function checkGuess() {
   if(document.getElementById('guessb').value == "Guess"){
     var current_guess = document.getElementById('guesst').value;
     if(random_number>current_guess){
-      document.getElementById('guessp').innerHTML = "That is too low!"
+      feedback(current_guess, "This is too low!")
     }
     else if(random_number<current_guess){
-      document.getElementById('guessp').innerHTML = "That is too high!"
+      feedback(current_guess, "This is too high!")
     }
-    else {
-      document.getElementById('guessp').innerHTML = "BOOM!"
+    else if( random_number == current_guess ){
+      feedback(current_guess, "BOOM!")
     }
   }
 
+}
+
+function feedback(current_guess, feedback) {
+  if(current_guess == "clearfeedback" && feedback == ""){
+    document.getElementById('guessp').innerHTML = feedback;
+  }
+  else {
+    document.getElementById('guessp').innerHTML = "Your last guess was " +  current_guess + ' ' + feedback;
+  }
 }
