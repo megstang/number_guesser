97ac40f60e319811a038c119c2b763ad16204209
Meghan Stang <mstang15@gmail.com>
2019-01-11 21:13:13 -0700

    add disable for clear and reset buttons

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,17 @@
-var random_number = randomNumber(1,100);
-
+var min = 1
+var max = 100
+var random_number = randomNumber(min,max);
 
 function clearGuess() {
-  document.getElementById('guesst').value = ''
+  document.getElementById('guesst').value = '';
+  enableClearButton();
 }
 
 function resetGuess() {
   random_number = randomNumber(1,100);
   feedback("clearfeedback", "");
+  clearGuess();
+  enableResetButton();
 }
 
 function randomNumber(min, max) {
@@ -17,7 +21,13 @@ function randomNumber(min, max) {
 function checkGuess() {
   if(document.getElementById('guessb').value == "Guess"){
     var current_guess = document.getElementById('guesst').value;
-    if(random_number>current_guess){
+    if(isNaN(current_guess)){
+      alert("Please input a number")
+    }
+    else if(current_guess < min || current_guess > max) {
+      alert( "Please enter a number between " + min + " and " + max)
+    }
+    else if(random_number>current_guess){
       feedback(current_guess, "This is too low!")
     }
     else if(random_number<current_guess){
@@ -26,6 +36,8 @@ function checkGuess() {
     else if( random_number == current_guess ){
       feedback(current_guess, "BOOM!")
     }
+    enableClearButton();
+    document.getElementById('reset-button').disabled = false;
   }
 }
 
@@ -36,4 +48,17 @@ function feedback(current_guess, feedback) {
   else {
     document.getElementById('guessp').innerHTML = "Your last guess was " +  current_guess + ' ' + feedback;
   }
+}
+
+function enableClearButton() {
+  if(document.getElementById('guesst').value == ''){
+    document.getElementById('clear-guess').disabled = true;
+  }
+  else {
+    document.getElementById('clear-guess').disabled = false;
+  }
+}
+
+function enableResetButton(){
+  document.getElementById('reset-button').disabled = true;
 }
